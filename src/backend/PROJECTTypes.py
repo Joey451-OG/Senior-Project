c249991e5b0b66ed5e8cfbebaae6c3cd8ac6b0f0
Joey Garneau <jmgarneau451@outlook.com>
2026-09-09 20:13:07 -0500

    Small changes, reformatting vars

--- src/backend/PROJECTTypes.py
+++ src/backend/PROJECTTypes.py
@@ -5,3 +5,9 @@ class TemperatureReading(NamedTuple):
     high: float | None
     critical: float | None
 
+class UsersSession(NamedTuple):
+    name: str | None
+    terminal: str | None
+    host: str | None
+    started: float | None
+    pid: int | None
--- src/backend/user-utils.py
+++ src/backend/user-utils.py
@@ -1,16 +1,17 @@
 import psutil
 import apscheduler
-import backend.PROJECTTypes as PTypes
+
+from backend.PROJECTTypes import TemperatureReading, UsersSession
 
 # Setup
 psutil.cpu_percent()
 CPU_SENSOR_NAME = ("coretemp", "k10temp", "cpu_thermal")
 
 
-def get_cpu_utilization() -> float:
+def getCpuUtilization() -> float:
     return psutil.cpu_percent()
 
-def get_cpu_temperatures() -> dict[str, list[PTypes.TemperatureReading]] | None:
+def getCpuTemperatures() -> dict[str, list[TemperatureReading]] | None:
     temps = psutil.sensors_temperatures()
 
     if temps:
@@ -22,25 +23,27 @@ def get_cpu_temperatures() -> dict[str, list[PTypes.TemperatureReading]] | None:
 
             for entry in entries:
 
-                ret_dictionary[entry.label] = PTypes.TemperatureReading(entry.current, entry.high, entry.critical)
+                ret_dictionary[entry.label] = TemperatureReading(
+                                                entry.current,
+                                                entry.high,
+                                                entry.critical
+                                            )
 
             return ret_dictionary
 
 
-def get_users() -> list[str | float | None]:
+def getUsers() -> list[str | float | None]:
     users = psutil.users()
     ret_list = []
 
     for u in users:
-        app = []
-
-        app.append(u.name)
-        app.append(u.terminal)
-        app.append(u.host)
-        app.append(u.started)
-        app.append(u.pid)
-
-        ret_list.append(app)
+        ret_list.append(UsersSession(
+            u.name, 
+            u.terminal, 
+            u.host, 
+            u.started, 
+            u.pid
+        ))
 
     return ret_list
 
